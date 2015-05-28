2130c4074ac17db86a7a41d33cf71ac87fb459e7
Mark DiFranco <mpdifran@gmail.com>
2015-05-28 15:20:25 -0400

    Initial Commit

---
+++ MyoSports/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyoSports
+//
+//  Created by Mark DiFranco on 2015-05-28.
+//  Copyright (c) 2015 MDF Projects. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
